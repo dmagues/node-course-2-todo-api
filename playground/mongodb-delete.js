c1aad0d5986c413a09a0221928c2e75bc11827c4
Daniel Magues <dmagues@campoecu.com>
2018-12-19 17:00:54 -0500

    delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,37 @@
+// object destructuring
+// const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect(
+  'mongodb://localhost:27017/TodoApp',
+  { useNewUrlParser: true },
+  (err, client) => {
+    if (err) return console.log('Unable to connect to MongoDb server');
+    console.log('Connected to MongoDb server');
+
+    const db = client.db('TodoApp');
+
+    //deleteMany
+    // db.collection('Todos')
+    //   .deleteMany({ text: 'Walk the dog' })
+    //   .then(result => {
+    //     console.log(result);
+    //   });
+
+    //deleteOne
+    // db.collection('Todos')
+    //   .deleteOne({ text: 'Walk the cat' })
+    //   .then(result => {
+    //     console.log(result);
+    //   });
+
+    //findOneanddelete
+    db.collection('Todos')
+      .findOneAndDelete({ completed: false })
+      .then(result => {
+        console.log(result);
+      });
+
+    client.close();
+  }
+);
