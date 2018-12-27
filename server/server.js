6a3bf89b06d3c247006cb3454f941d525d9e2eaf
Daniel Magues <dmagues@campoecu.com>
2018-12-27 09:16:28 -0500

    delete todos/:id

--- server/server.js
+++ server/server.js
@@ -38,13 +38,26 @@ app.get('/todos', (req, res) => {
 });
 
 app.get('/todos/:id', (req, res) => {
-  var id = req.params['id'];
+  var id = req.params.id;
   if (!ObjectID.isValid(id)) return res.status(404).send();
 
   Todo.findById(id)
     .then(todo => {
       if (!todo) return res.status(404).send();
       res.status(200).send({ todo });
+    })
+    .catch(e => {
+      res.status(400).send();
+    });
+});
+
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) return res.status(404).send();
+  Todo.findByIdAndDelete(id)
+    .then(doc => {
+      if (!doc) return res.status(404).send();
+      res.send({ doc });
     })
     .catch(e => {
       res.status(400).send();
