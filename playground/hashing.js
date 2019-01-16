d71cf3d0d36cbee2291d4db1cbecc5fac9427dc9
Daniel Magues <dmagues@campoecu.com>
2019-01-16 16:08:37 -0500

    hash password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,31 @@
 const { SHA256 } = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decode', decoded);
+var hashedPassword =
+  '$2a$10$BhY8NgJ8x77Grwec2Jx14uVVvR7QlhGlhA/CGUYzTg1Caogm85nNO';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
+
+// var data = {
+//   id: 10
+// };
+
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decode', decoded);
 
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
